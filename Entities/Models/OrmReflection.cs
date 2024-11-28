60b5053455a95c3da90eaae39a7497ef0555da32
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2024-11-28 08:31:54 +0100

    Add project files.

---
+++ Entities/Models/OrmReflection.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    public static class OrmReflection
+    {
+        private static SqlConnection? DatabaseConnection = null;
+
+        public static int InsertObjectToDatabase<T>(this T obj, string tableName)
+        {
+            int Result = 0;
+            // Get the type of the object
+            Type type = typeof(T);
+
+            // Get all public instance properties
+            PropertyInfo[] properties = type.GetProperties();
+
+            PropertyInfo primaryKeyProperty = properties.FirstOrDefault(p => p.GetCustomAttributes(typeof(KeyAttribute), true).Length > 0);
+
+            // Create a SQL INSERT statement
+            StringBuilder sql = new StringBuilder();
+            sql.Append($"INSERT INTO {tableName} (");
+
+            // Build the column list and parameter placeholders
+            List<string> columnNames = new List<string>();
+            List<object> parameters = new List<object>();
+            foreach (PropertyInfo property in properties)
+            {
+                // Filter properties based on your criteria (e.g., exclude navigation properties)
+                if (property != primaryKeyProperty && (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string)))
+                {
+                    columnNames.Add(property.Name);
+                    parameters.Add(property.GetValue(obj));
+                }
+            }
+
+            sql.Append(string.Join(", ", columnNames));
+            sql.Append(") VALUES (");
+            sql.Append(string.Join(", ", parameters.Select(p => "@p" + parameters.IndexOf(p))));
+            sql.Append(");");
+
+            // Execute the SQL query with parameters
+            using (SqlConnection connection = new SqlConnection(GetSqlConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql.ToString(),
+                        connection))
+                {
+                    for (int i = 0; i < parameters.Count; i++)
+                    {
+                        command.Parameters.AddWithValue("@p" + i, parameters[i]);
+                    }
+
+                    //OpenDatabaseConnection();
+                    connection.Open();
+                    Result = command.ExecuteNonQuery();
+                    if (Result < 0)
+                    {
+                        Console.WriteLine("Noget gik galt under Save operationen !!!");
+                    }
+                }
+                //CloseDatabaseConnection();
+                connection.Close();
+            }
+
+            return Result;
+        }
+
+        public static int DeleteObjectToDatabase<T>(this T obj, string tableName)
+        {
+            int Result = 0;
+            // Get the type of the object
+            Type type = typeof(T);
+
+            // Get all public instance properties
+            PropertyInfo[] properties = type.GetProperties();
+
+            PropertyInfo primaryKeyProperty = properties.FirstOrDefault(p => p.GetCustomAttributes(typeof(KeyAttribute), true).Length > 0);
+
+
+            // Create a SQL INSERT statement
+            StringBuilder sql = new StringBuilder();
+            sql.Append($"Delete {tableName}");
+            sql.Append($" Where {primaryKeyProperty.Name} = {primaryKeyProperty.GetValue(obj)}");
+     
+
+            // Execute the SQL query with parameters
+            using (SqlConnection connection = new SqlConnection(GetSqlConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql.ToString(),
+                        connection))
+                {
+                    //OpenDatabaseConnection();
+                    connection.Open();
+                    Result = command.ExecuteNonQuery();
+                    if (Result < 0)
+                    {
+                        Console.WriteLine("Noget gik galt under Save operationen !!!");
+                    }
+                }
+                //CloseDatabaseConnection();
+                connection.Close();
+            }
+
+            return Result;
+        }
+        public static List<T> GetObjectToDatabase<T>(this T obj,string tableName) where T : new()
+        {
+            int Result = 0;
+            // Get the type of the object
+            Type type = typeof(T);
+
+            // Get all public instance properties
+            List<T> GenericList = new List<T>();
+            var Entity = typeof(T);
+            var PropDict = new Dictionary<string, PropertyInfo>();
+            PropertyInfo[] properties = type.GetProperties();
+
+            PropertyInfo primaryKeyProperty = properties.FirstOrDefault(p => p.GetCustomAttributes(typeof(KeyAttribute), true).Length > 0);
+
+
+            // Create a SQL select statement
+            StringBuilder sql = new StringBuilder();
+            sql.Append($"Select * from {tableName}");
+            sql.Append($" Where {primaryKeyProperty.Name} = {primaryKeyProperty.GetValue(obj)}");
+
+
+            // Execute the SQL query with parameters
+            using (SqlConnection connection = new SqlConnection(GetSqlConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql.ToString(),
+                        connection))
+                {
+                    //OpenDatabaseConnection();
+                    connection.Open();
+                    SqlDataReader DataReader = command.ExecuteReader();
+
+
+                    if (DataReader.HasRows)
+                    {
+                        var Props = Entity.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                        PropDict = Props.ToDictionary(p => p.Name.ToUpper(), p => p);
+
+                        while (DataReader.Read())
+                        {
+                            T newObject = new T();
+
+                            for (int Index = 0; Index < DataReader.FieldCount; Index++)
+                            {
+                                if (PropDict.ContainsKey(DataReader.GetName(Index).ToUpper()))
+                                {
+                                    var Info = PropDict[DataReader.GetName(Index).ToUpper()];
+
+                                    if ((Info != null) && Info.CanWrite)
+                                    {
+                                        var Val = DataReader.GetValue(Index);
+                                        Info.SetValue(newObject, (Val == DBNull.Value) ? null : Val, null);
+                                    }
+                                }
+                            }
+
+                            GenericList.Add(newObject);
+
+                            //mappedDataList.Add(mappedDataRow);
+                        }
+                    }
+                }
+
+                return GenericList;
+            }
+        }
+
+
+            private static void OpenDatabaseConnection()
+        {
+            if (null == DatabaseConnection)
+            {
+                DatabaseConnection = new SqlConnection(GetSqlConnectionString());
+
+                DatabaseConnection.Open();
+            }
+        }
+
+        private static void CloseDatabaseConnection()
+        {
+            if (null != DatabaseConnection)
+            {
+                DatabaseConnection.Close();
+                DatabaseConnection = null;
+            }
+        }
+
+        private static string GetSqlConnectionString()
+        {
+            // Prepare the connection string to Azure SQL Database.
+            var sqlConnectionSB = new SqlConnectionStringBuilder
+            {
+                // Change these values to your values.
+                DataSource = "LAPTOP-COT3JNJ0", //["Server"]
+                InitialCatalog = "Student_WebApi_Core_8_0",                                       //["Database"]
+                UserID = "Username2",                                          // "@yourservername"  as suffix sometimes.
+                Password = "Password2",
+                TrustServerCertificate = true,
+            };
+            return sqlConnectionSB.ToString();
+        }
+    }
+}
---
+++ Student_WebApi_ADO_NET/Controllers/StudentController_ADO_Net_Reflection.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Contracts;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Mapster;
+using System.Net;
+using Student_WebApi_ADO_NET.ViewModels;
+
+namespace Student_WebApi_ADO_NET.Controllers
+{
+    public class StudentController_ADO_Net_Reflection : Controller
+    {
+        private ILoggerManager _logger;
+
+        public StudentController_ADO_Net_Reflection(ILoggerManager logger)
+        {
+            this._logger = logger;
+        }
+
+        // POST: api/Student
+        [HttpPost("CreateStudent_ADO_Net_Reflection")]
+        public async Task<IActionResult> CreateStudent_ADO_Net_Reflection([FromBody] StudentForSaveDto StudentForSaveDto_Object,
+                                                                           string UserName = "No Name")
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    this._logger.LogError($"ModelState is Invalid for {UserName} in action CreateStudent_ADO_Net_Reflection");
+                    return BadRequest(ModelState);
+                }
+
+                Student Student_Object = new Student();
+                Student_Object = StudentForSaveDto_Object.Adapt<Student>();
+
+                // Fuld Generisk metode
+                int SaveResult = Student_Object.InsertObjectToDatabase<Student>(Student.TABLE_NAME);
+
+                if (SaveResult >= 0)
+                {
+                    return Ok($"Student : {Student_Object.StudentName} oprettet !!!");
+                }
+                else
+                {
+                    return BadRequest($"Noget gik galt, da {Student_Object.StudentName} : skulle oprettes !!!");
+                }
+            }
+            catch (Exception Error)
+            {
+                _logger.LogError($"Something went wrong inside CreateStudent_ADO_Net_Reflection action for {UserName}: {Error.Message}");
+                return StatusCode((int)HttpStatusCode.InternalServerError, $"Internal server error : {Error.ToString()}");
+            }
+        }
+        [HttpDelete("DeleteStudent_ADO_Net_Reflection")]
+        public async Task<IActionResult> DeleteStudent_ADO_Net_Reflection(int StudentId,
+                                                                           string UserName = "No Name")
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    this._logger.LogError($"ModelState is Invalid for {UserName} in action CreateStudent_ADO_Net_Reflection");
+                    return BadRequest(ModelState);
+                }
+
+                Student Student_Object = new Student();
+                Student_Object.StudentID = StudentId;
+
+                // Fuld Generisk metode
+                int SaveResult = Student_Object.DeleteObjectToDatabase<Student>(Student.TABLE_NAME);
+
+                if (SaveResult >= 0)
+                {
+                    return Ok($"Student : {Student_Object.StudentName} Deletet !!!");
+                }
+                else
+                {
+                    return BadRequest($"Noget gik galt, da {Student_Object.StudentName} : skulle DEleted !!!");
+                }
+            }
+            catch (Exception Error)
+            {
+                _logger.LogError($"Something went wrong inside DeleteStudent_ADO_Net_Reflection action for {UserName}: {Error.Message}");
+                return StatusCode((int)HttpStatusCode.InternalServerError, $"Internal server error : {Error.ToString()}");
+            }
+        }
+        [HttpGet("GEtStudent_ADO_Net_Reflection")]
+        public async Task<IActionResult> GetStudent_ADO_Net_Reflection(int StudentId,
+                                                                           string UserName = "No Name")
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    this._logger.LogError($"ModelState is Invalid for {UserName} in action CreateStudent_ADO_Net_Reflection");
+                    return BadRequest(ModelState);
+                }
+
+                Student Student_Object = new Student();
+                Student_Object.StudentID=StudentId;
+
+                IEnumerable<Student> Students = new List<Student>();
+                // Fuld Generisk metode
+                Students = Student_Object.GetObjectToDatabase<Student>(Student.TABLE_NAME);
+
+                return Ok(Students);
+            }
+            catch (Exception Error)
+            {
+                _logger.LogError($"Something went wrong inside CreateStudent_ADO_Net_Reflection action for {UserName}: {Error.Message}");
+                return StatusCode((int)HttpStatusCode.InternalServerError, $"Internal server error : {Error.ToString()}");
+            }
+        }
+    }
+}
